cbe5d43aa667d920502702b17659c324f6587c2a
angelSantori <antonyo.da.mm@gmail.com>
2023-11-15 11:45:13 -0700

    Nuevo usuario funciona POST sólo con llave {}

--- routes/routes.js
+++ routes/routes.js
@@ -38,6 +38,33 @@ const router = app => {
             response.status(500).send('Error de servidor');
         }
     });
+
+    //Agregar un nuevo usuario
+    app.post('/users', async (request, response) => {
+        const newUser = request.body;
+
+        try {
+            const pool = await sql.connect('./data/config');
+
+            const request = pool.request();
+            
+            // Asignar parámetros individualmente
+            request.input('userNombre', sql.VarChar(45), newUser.userNombre);
+            request.input('userEdad', sql.Int, newUser.userEdad);
+            request.input('userEmail', sql.VarChar(320), newUser.userEmail);
+            request.input('userPass', sql.VarChar(128), newUser.userPass);
+
+            const result = await request.query('INSERT INTO users (userNombre, userEdad, userEmail, userPass) OUTPUT INSERTED.idusers VALUES (@userNombre, @userEdad, @userEmail, @userPass)');
+
+            const insertId = result.recordset[0].idusers;
+
+            response.status(201).send(`User added with ID: ${insertId}`);
+        } catch (error) {
+            console.error(error);
+            response.status(500).send('Error de servidor');
+        }
+    });
+    
 }
 
 //Exportar el router
