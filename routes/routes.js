17dad6674534b54387b733056ab3bb3be7198253
angelSantori <antonyo.da.mm@gmail.com>
2023-11-15 13:09:44 -0700

    Rutas p/productos OK

--- routes/routes.js
+++ routes/routes.js
@@ -114,7 +114,109 @@ const router = app => {
             console.error(error);
             response.status(500).send('Error de servidor');
         }
-    });    
+    });
+    
+    //-----------------------------------------------------------------------------------------------------------------------Productos
+    //Mostrar todos los productos
+    app.get('/products', async (request, response) => {
+        try {
+            const pool = await sql.connect('./data/config');
+            const result = await pool.request().query('SELECT * FROM products');
+            response.send(result.recordset);
+        } catch (error) {
+            console.error(error);
+            response.status(500).send('Error de servidor');
+        }
+    });
+
+    // Mostrar un solo producto por ID
+    app.get('/products/:id', async (request, response) => {
+        const id = request.params.id;
+
+        try {
+            const pool = await sql.connect('./data/config');
+            const result = await pool
+                .request()
+                .input('id', sql.Int, id) // Declarar el parámetro @id
+                .query('SELECT * FROM products WHERE idproduct = @id');
+            
+            response.send(result.recordset);
+        } catch (error) {
+            console.error(error);
+            response.status(500).send('Error de servidor');
+        }
+    });
+
+    //Agregar un nuevo usuario
+    app.post('/products', async (request, response) => {
+        const newProduct = request.body;
+
+        try {
+            const pool = await sql.connect('./data/config');
+
+            const request = pool.request();
+            
+            // Asignar parámetros individualmente
+            request.input('productNombre', sql.VarChar(45), newProduct.productNombre);
+            request.input('productDescription', sql.VarChar(320), newProduct.productDescription);
+            request.input('productPrecio', sql.Float, newProduct.productPrecio);
+            request.input('productExistencias', sql.Int, newProduct.productExistencias);                        
+            
+            const result = await request.query('INSERT INTO products (productNombre, productDescription, productPrecio, productExistencias) OUTPUT INSERTED.idproduct VALUES (@productNombre, @productDescription, @productPrecio, @productExistencias)');
+
+            const insertId = result.recordset[0].idproduct;
+
+            response.status(201).send(`Product added with ID: ${insertId}`);
+        } catch (error) {
+            console.error(error);
+            response.status(500).send('Error de servidor');
+        }
+    });
+
+    // Actualizar un producto existente
+    app.put('/products/:id', async (request, response) => {
+        try {
+            const id = request.params.id;
+            const newProduct = request.body;
+
+            const pool = await sql.connect('./data/config');
+
+            const result = await pool
+                .request()
+                .input('idproduct', sql.Int, id)
+                .input('productNombre', sql.VarChar(45), newProduct.productNombre)
+                .input('productDescription', sql.VarChar(320), newProduct.productDescription)
+                .input('productPrecio', sql.Float, newProduct.productPrecio)
+                .input('productExistencias', sql.Int, newProduct.productExistencias)
+
+                .query('UPDATE products SET productNombre = @productNombre, productDescription = @productDescription, productPrecio = @productPrecio, productExistencias = @productExistencias WHERE idproduct = @idproduct');
+
+            response.send('Product updated successfully');
+        } catch (error) {
+            console.error(error);
+            response.status(500).send('Error de servidor');
+        }
+    });
+
+    // Eliminar un producto
+    app.delete('/products/:id', async (request, response) => {
+        const id = request.params.id;
+
+        try {
+            const pool = await sql.connect('./data/config');
+
+            const result = await pool
+                .request()
+                .input('id', sql.Int, id)
+                .query('DELETE FROM products WHERE idproduct = @id');
+
+            response.send('Product deleted');
+        } catch (error) {
+            console.error(error);
+            response.status(500).send('Error de servidor');
+        }
+    });
+    //-----------------------------------------------------------------------------------------------------------------------/Productos
 
 }
 
